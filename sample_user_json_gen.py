0d27e66fd5b02145f2c3968bda68006cc34660e7
Fiona Pye <fiona.pye@outlook.com>
2021-09-13 22:10:01 +0100

    add sample_user_json_gen.py to help people make the right data to run the app

---
+++ sample_user_json_gen.py
@@ -0,0 +1,37 @@
+#user weburl and pushover tokens config
+
+import os
+import json
+import platform
+
+
+# paths between platforms
+if platform.system() == 'Windows':
+    wdir = 'D:\\GitHub\\Bindicator'
+elif platform.system() == 'Linux':
+    wdir = '/home/pi/Documents/Bindicator'
+
+# make a folder called user_config and ENSURE this is in the gitignore
+#   DO NOT COMMIT YOUR USER DATA TO GITHUB!
+os.chdir(os.path.join(wdir,'user_config'))
+
+
+def write_json (filename, data):
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile)
+
+
+# add your data here
+users = {
+    "<user1>" : {"urlpath":"<add_house_bin_collection_url>"
+        , "app" : "<add_bindicator_pushover_app_token>"
+        , "user" : "<add_user_pushover_token>"
+        },
+    "[user2]" : {"urlpath":"<add_house_bin_collection_url>"
+        , "app" : "<add_bindicator_pushover_app_token>"
+        , "user" : "<add_user_pushover_token>"
+        }
+}
+
+
+write_json('users.json', users)
